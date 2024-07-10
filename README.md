ed0064452b7f84ce5a932eeea91f33261a664c74
ejazulhaque0 <ejazulhaque09@gmail.com>
2024-07-10 13:28:35 +0530

    Updated the UI and ReadMe file also added comments for better understanding

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Weather-Forecast
 This Weather Forecast Application provides real-time  and extended weather information for various locations. It is built using JavaScript, HTML, and Tailwind CSS, and the WeatherAPI for fetching weather data.
+
+## Features
+
+- "Search by city": Users can enter a city name to fetch weather information.
+- "Current Location": Users can use their current location to get weather information.
+- "Weather Display": Displays current weather, temperature, wind speed, and humidity.
+- "5-Day Forcast": Provides a forecast for the next 5 days with weather conditions, temperature, wind speed, and humidity.
+
+## Usage
+1.Open the 'index.html' file in your web browser.
+
+## Dependencies
+- Tailwind CSS -  For styling the application
+- Weather API-  Used to fetch weather data (WeatherAPI)
+
+## Project Structure 
+- 'index.html' - The HTML file containing the structure of the application. 
+- 'style.css' - Custom CSS file for additional styling.
+- 'script.js' - JavaScript file to handle API calls and dynamic updates to the DOM. 
+
+## Setup Instructions
+1. **Clone the repository**
+
+```bash
+
+git clone https://github.com/ejazulhaque09/Weather-Application.git
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+// API key
 const apiKey = 'c05fb24f435f452baba50023240607';
+
+// Dom elements
 const searchBtn = document.getElementById('searchBtn');
 const cityInput = document.getElementById('cityInput');
 const currentLocationBtn = document.getElementById('currentLocationBtn');
@@ -11,17 +15,21 @@ const errorMessage = document.getElementById('errorMessage');
 const recentCitiesDropdown = document.getElementById('recentCities')
 const loadingMessage = document.getElementById('loadingMessage')
 
+
+// Event listener for search button
 searchBtn.addEventListener('click', () => {
     const cityName = cityInput.value.trim();
     if (cityName) {
         fetchWeatherDataByCity(cityName);
-        addCityToRecent(cityName);  // will add city to recent
+        addCityToRecent(cityName);  // will add city to recent cities list
     }
     else {
-        showError('Please enter a city name.')
+        showError('Please enter a city name.')  // Shows error if no city name entered
     }
 });
 
+
+// Event listener for the current location button
 currentLocationBtn.addEventListener('click', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
@@ -37,12 +45,15 @@ currentLocationBtn.addEventListener('click', () => {
 });
 
 
+
+// Event listener for recent cities dropdown
 recentCitiesDropdown.addEventListener('change', () => {
     const cityName = recentCitiesDropdown.value;
     fetchWeatherDataByCity(cityName);
 })
 
 
+// Function to fetch weather data by city name
 function fetchWeatherDataByCity(city) {
 
     showLoading();
@@ -53,6 +64,8 @@ function fetchWeatherDataByCity(city) {
         .finally(hideLoading);
 }
 
+
+// Function to fetch weather data by coordinates
 function fetchWeatherDataByCoordinates(latitude, longitude) {
 
     showLoading();
@@ -64,48 +77,65 @@ function fetchWeatherDataByCoordinates(latitude, longitude) {
 
 }
 
-function displayWeatherData(data) {
-    weatherData.classList.remove('hidden'); // Displaying the weather Data
-    errorMessage.classList.add('hidden');  
 
-    locationName.textContent = `${data.location.name}, ${data.location.country}`;
+// Function to display the weather data
+function displayWeatherData(data) {
+
+    console.log(data);
+    weatherData.classList.remove('hidden'); // Display the weather Data
+    errorMessage.classList.add('hidden'); // Hide the error messages
+    locationName.innerHTML = `<p>${data.location.name}</p>
+                              <p>${data.location.country}</p>
+                              </p>${data.location.localtime}</p>`;
     currentWeatherIcon.src = `https:${data.current.condition.icon}`;
-    CurrentWeatherText.textContent = `Current: ${data.current.temp_c}°C,${data.current.condition.text}`;
+    CurrentWeatherText.innerHTML = `<div class="p-4 rounded-lg mx-2">
+                                    <p>Current: ${data.current.temp_c} °C"</p>
+                                    <p">${data.current.condition.text}</p>
+                                    <p>Wind: ${data.current.wind_kph} kmph</p>
+                                    <p>Humidity: ${data.current.humidity} %</p>
+                                    </div>`;
 
     extendedForecast.innerHTML = data.forecast.forecastday.map(day => `
-        <div class="p-4 bg-grey-100 rounded-lg shadow-md">
-            <div class="flex flex-col items-center">
+        <div class="rounded-lg border m-1">
+            <div class="pb-1 flex flex-col items-center ">
+                <p class="mt-1 font-bold">${day.date}<p>
                 <img src="https:${day.day.condition.icon}" alt="weather icon" class="w-16 h-16">
-                <p class="mt-2">${day.date}<p>
-                <p>${day.day.avgtemp_c}°C, ${day.day.condition.text} </p>
+                <!-- <p>Sunrise: ${day.astro.sunrise}<p> -->
+                <p>${day.day.avgtemp_c}°C</p>
+                <p>${day.day.condition.text} </p>
                 <p>Wind: ${day.day.maxwind_kph} kmph</p>
-                <p>Humidity: ${day.day.avghumidity}%</p>
+                <p>Humidity: ${day.day.avghumidity} %</p>
             </div>
         </div>`).join('');
 }
 
-function showError(message){
+
+// Function to show error message
+function showError(message) {
     weatherData.classList.add('hidden');
     errorMessage.classList.remove('hidden');
     errorMessage.textContent = message;
 }
 
-function addCityToRecent(city){
+
+// Fucntion to add city
+function addCityToRecent(city) {
     let recentCities = JSON.parse(localStorage.getItem('recentCities')) || [];
 
-    if(!recentCities.includes(city)) {
-        if(recentCities.length >= 5){  // maximum cities in recent will be 5
+    if (!recentCities.includes(city)) {
+        if (recentCities.length >= 5) {  // maximum cities in recent will be 5
             recentCities.pop();
-        } 
+        }
         recentCities.unshift(city);  // adding cities
-        localStorage.setItem('recentCities',JSON.stringify(recentCities));
+        localStorage.setItem('recentCities', JSON.stringify(recentCities));
         updateRecentCitiesDropdown();
     }
 }
 
 
-function updateRecentCitiesDropdown(){
-    let recentCities = JSON.parse(localStorage.getItem('recentCities')) || [];  
+// Function to update recent cities dropdown
+function updateRecentCitiesDropdown() {
+    let recentCities = JSON.parse(localStorage.getItem('recentCities')) || [];
     recentCitiesDropdown.innerHTML = '<option value="">Select a recent city </option>'
     recentCities.forEach(city => {
         const option = document.createElement('option');
@@ -114,7 +144,7 @@ function updateRecentCitiesDropdown(){
         recentCitiesDropdown.appendChild(option);
     });
 
-    if(recentCities.length === 0){
+    if (recentCities.length === 0) {
         recentCitiesDropdown.classList.add('hidden')
     }
     else {
@@ -122,11 +152,17 @@ function updateRecentCitiesDropdown(){
     }
 }
 
-function showLoading(){
+
+// Function to show the loading message
+function showLoading() {
     loadingMessage.classList.remove('hidden')
 }
-function hideLoading(){
+
+// Function to hide the loading message
+function hideLoading() {
     loadingMessage.classList.add('hidden')
 }
 
+
+//Updates the recent cities dropdown when the page loads
 updateRecentCitiesDropdown();
